8f267bf74d141b3d0492eacc6777a33ce7076141
knowledge91 <dirkhornung91@gmail.com>
2018-05-20 19:11:02 +0200

    added announceMeeting Front-End

--- src/components/announceMeeting.js
+++ src/components/announceMeeting.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
-import { Form, Radio, Button } from 'antd'
+import { Form, Radio, Button, DatePicker, TimePicker, Row, Col, Input, Upload, Icon, message } from 'antd'
 import { HOCForm } from '../containers/addCompanyForms.js'
 import AgreementSelector from './agreementSelector.js'
 const FormItem = Form.Item
+const { TextArea } = Input
 const RadioButton = Radio.Button
 const RadioGroup = Radio.Group
 
@@ -18,8 +19,26 @@ class AnnounceMeeting extends Component {
     const { form } = this.props
     const { getFieldDecorator } = form
 
+    const uploadProps = {
+      name: 'file',
+      action: '//jsonplaceholder.typicode.com/posts/',
+      headers: {
+        authorization: 'authorization-text',
+      },
+      onChange(info) {
+        if (info.file.status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      },
+    };
+
     return (
-      <Form onSubmit={this.handleSubmit}>
+      <Form onSubmit={this.handleSubmit} layout='inline' >
         <FormItem>
           {getFieldDecorator('meetingType', {
              rules: [{ required: true, message: 'Es obligatorio.' }],
@@ -30,13 +49,67 @@ class AnnounceMeeting extends Component {
              </RadioGroup>
            )}
         </FormItem>
+        <Row type="flex">
+          <Col>
+            <FormItem label="Fecha Inicio:">
+              {getFieldDecorator('startDate', {
+                 rules: [{ required: true, message: 'Es obligatorio.' }],
+              })(<DatePicker />)}
+            </FormItem>
+          </Col>
+          <Col>
+            <FormItem>
+              {getFieldDecorator('startTime', {
+                 rules: [{ required: true, message: 'Es obligatorio.' }],
+              })(<TimePicker />)}
+            </FormItem>
+          </Col>
+        </Row>
 
-        <AgreementSelector
-          form={form}
-          fieldId="agreementTypes" />
+        <Row type="flex">
+          <Col>
+            <FormItem label="Fecha Final:">
+              {getFieldDecorator('endDate', {
+                 rules: [{ required: true, message: 'Es obligatorio.' }],
+              })(<DatePicker />)}
+            </FormItem>
+          </Col>
+          <Col>
+            <FormItem>
+              {getFieldDecorator('endTime', {
+                 rules: [{ required: true, message: 'Es obligatorio.' }],
+              })(<TimePicker />)}
+            </FormItem>
+          </Col>
+        </Row>
+
+        <Row type="flex">
+          <Col>
+            <AgreementSelector
+              form={form}
+              fieldId="agreementTypes"
+            />
+          </Col>
+        </Row>
+
+        <Row type="flex">
+          <Col>
+            <FormItem label="Informacion Addicional">
+              {getFieldDecorator('additionalInfo', {
+                 rules: [{ required: true, message: 'Es obligatorio.' }],
+              })(<TextArea autosize />)}
+            </FormItem>
+          </Col>
+        </Row>
+
+        <Upload {...uploadProps}>
+          <Button>
+            <Icon type="upload" /> Click to Upload
+          </Button>
+        </Upload>
 
         <Button type="primary" htmlType="submit">
-          Convocar
+          Enviar Convocatoria
         </Button>
 
         <pre>
