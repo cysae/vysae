a4d559009810dade936f99a6ea6dd57b0510aeba
knowledge91 <dirkhornung91@gmail.com>
2018-06-19 10:57:01 +0200

    vote cleanup

--- src/containers/meetingVote.js
+++ src/containers/meetingVote.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 import { Form, List, Radio, Button } from 'antd'
 import { Link } from 'react-router-dom'
 // redux
@@ -30,56 +30,41 @@ function HOCForm(formComponent) {
     onValuesChange(_, values) {
       /* console.log(values); */
     },
-  })(formComponent))
+  })(dispatch => ({
+    requestVote: vote => dispatch(requestVote(vote))
+  }))(formComponent))
 }
 
-class meetingVote extends Component {
-  state = {
-    agreementTypes: [
-      'Aumento o reducción de capital',
-      'Autorización a administradores para que se dediquen a actividad inmersa en el objecto social',
-      'Autorización a administradores para que se dediquen a actividad inmersa en el objeto social',
-      'Exclusión y separación de socios',
-      'Cambio de domicilio',
-      'Supresión o limitación del derecho de prederencia en aumentos de capital',
-      'Modificación estructural',
-      'Cesión global de activo y pasivo',
-    ]
-  }
-
-  render() {
-    const { requestVote, meeting, form } = this.props
-    const { agreementTypes } = meeting
-    const { getFieldDecorator } = form
-    return (
-      <Fragment>
-        <Form layout="vertical" onSubmit={this.handleSubmit}>
-          <List
-            header={<div>CONVOCATORIA A LA JUNTA GENERAL DE SOCIOS</div>}
-            footer={<Button><Link to="/meetings/result">Votar</Link></Button>}
-            bordered
-            dataSource={agreementTypes}
-            renderItem={item => (
-              <List.Item actions={[
-                <FormItem>
-                  {getFieldDecorator(item, {
-                     rules: [{ required: true, message: 'Es obligatorio.' }],
-                  })(
-                     <RadioGroup>
-                       <RadioButton value="yes">Sí</RadioButton>
-                       <RadioButton value="blank">En blanco</RadioButton>
-                       <RadioButton value="no">No</RadioButton>
-                     </RadioGroup>
-                   )}
-                </FormItem>
-              ]}>
-                {item}
-              </List.Item>)}
-          />
-        </Form>
-      </Fragment>
-    )
-  }
+const meetingVote = props => {
+  const { requestVote, meeting, form } = this.props
+  const { agreementTypes } = meeting
+  const { getFieldDecorator } = form
+  return (
+    <Form layout="vertical" onSubmit={this.handleSubmit}>
+      <List
+        header={<div>CONVOCATORIA A LA JUNTA GENERAL DE SOCIOS</div>}
+        footer={<Button><Link to="/meetings/result">Votar</Link></Button>}
+        bordered
+        dataSource={agreementTypes}
+        renderItem={item => (
+          <List.Item actions={[
+            <FormItem>
+              {getFieldDecorator(item, {
+                 rules: [{ required: true, message: 'Es obligatorio.' }],
+              })(
+                 <RadioGroup>
+                   <RadioButton value="yes">Sí</RadioButton>
+                   <RadioButton value="blank">En blanco</RadioButton>
+                   <RadioButton value="no">No</RadioButton>
+                 </RadioGroup>
+               )}
+            </FormItem>
+          ]}>
+            {item}
+          </List.Item>)}
+      />
+    </Form>
+  )
 }
 
 
