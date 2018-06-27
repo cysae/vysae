2ad7d458667039c466fa0cafcfe0a16197e36fad
knowledge91 <dirkhornung91@gmail.com>
2018-06-27 15:05:23 +0200

    promisified graphql

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,12 +1,16 @@
 var AWS = require('aws-sdk');
 import dynamodb from 'serverless-dynamodb-client';
+import Promise from 'Bluebird'
 
 let docClient;
-console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
     docClient = new AWS.DynamoDB.DocumentClient();
 } else {
-    docClient = dynamodb.doc;
+    docClient = new AWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+    })
+    Promise.promisifyAll(Object.getPrototypeOf(docClient));
 }
 
 const promisify = foo =>
@@ -15,8 +19,12 @@ const promisify = foo =>
               if (error) {
                   reject(error);
               } else {
+                  console.log(result)
                   if(result.Items.length > 0) {
-                      resolve({ name: result.Items[0].name})
+                      resolve({
+                          id: result.Items[0].PK.slice(-36),
+                          name: result.Items[0].name
+                      })
                   } else {
                       resolve(null);
                   }
@@ -24,15 +32,19 @@ const promisify = foo =>
           });
       });
 
+
 export const resolvers = {
-    Query: { getCompany: (root, args) => getCompany(args)},
+    Query: {
+        getCompany: (root, args) => getCompany(args),
+        getShareholder: (root, args) => getShareholder(args),
+    },
 };
 
 function getCompany(args) {
     return promisify(callback =>
                      docClient.query(
                          {
-                             TableName: 'vysae',
+                             TableName: 'Vysae',
                              KeyConditionExpression: '#name = :v1',
                              ExpressionAttributeNames:{
                                  "#name": "name"
@@ -44,4 +56,19 @@ function getCompany(args) {
                          callback
                      )
                     )
+}
+
+function getShareholder(args) {
+    return docClient.queryAsync(
+        {
+            TableName: 'Vysae',
+            KeyConditionExpression: 'PK = :v1',
+            ExpressionAttributeValues: {
+                ':v1': `Shareholder-${args.id}`,
+            },
+        },
+    ).then(res => {
+        const item = res.Items[0]
+        return { id: item.PK.slice(-36) }
+    }).catch(err => console.log(err))
 }
--- backend/schema.js
+++ backend/schema.js
@@ -1,5 +1,11 @@
 export const typeDefs = `
-  type Query { getCompany(name: String!): Company }
+  type Query {
+    getCompany(name: String!): Company
+    getShareholder(id: String!): Shareholder
+  }
   type Company { name: String! }
-  type Shareholder { id: String! }
+  type Shareholder {
+    id: String!
+    companies: [Company]
+  }
 `
