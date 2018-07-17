b99eb6033f1533a952c78d4996c741079748ff67
knowledge91 <dirkhornung91@gmail.com>
2018-07-17 23:11:28 +0200

    added agreements & votes

--- backend/seedData/createSeedData.js
+++ backend/seedData/createSeedData.js
@@ -4,11 +4,51 @@ const uuidv4 = require('uuid/v4');
 
 const numCompanies = 10;
 const numShareholders = 2;
+const numMeetings = 2;
+const numAgreements = 5;
+const numVotes = 10;
 
 const vData = [];
 const companies = [];
+const shareholders = [];
+const meetings = [];
+const agreements = [];
+const votes = []
+
+const agreementTypes = [
+    'Aumento o reducción de capital',
+    'Autorización a administradores para que se dediquen a actividad inmersa en el objecto social',
+    'Autorización a administradores para que se dediquen a actividad inmersa en el objeto social',
+    'Exclusión y separación de socios',
+    'Cambio de domicilio',
+    'Supresión o limitación del derecho de prederencia en aumentos de capital',
+    'Modificación estructural',
+    'Cesión global de activo y pasivo',
+]
 
 faker.seed(1000);
+
+
+function addTo(type, obj) {
+    let i = 0; toAdd = {};
+    switch(type) {
+    case "company":
+        i = Math.round(Math.random()*(numCompanies-1));
+        toAdd = {...obj, PK: companies[i].PK}
+        break;
+    case "meeting":
+        i = Math.round(Math.random()*(numMeetings-1));
+        toAdd = {...obj, PK: meetings[i].PK}
+        break;
+    case "agreement":
+        i = Math.round(Math.random()*(numAgreements-1));
+        toAdd = {...obj, PK: agreements[i].PK}
+        break;
+    default:
+        break;
+    }
+    vData.push(toAdd)
+}
 
 // create Companies
 for (let i = 0; i < numCompanies; i++) {
@@ -34,19 +74,59 @@ for (let i = 0; i < numShareholders; i++) {
     const shareholder = {
         PK, SK, name
     }
+    shareholders.push(shareholder)
     vData.push(shareholder)
 
-    // add to company
-    const companyNum = Math.round(Math.random()*(numCompanies-1));
-    for(let i=0; i < companyNum; i++) {
-        const company = companies[i]
-        const cShareholder = {
-            PK: company.PK,
-            SK: shareholder.SK,
-            name
-        }
-        vData.push(cShareholder)
+    // add to n companies randomly
+    const n = Math.round(Math.random()*(numCompanies-1));
+    for(let j=0; j<n; j++) {
+        addTo('company', shareholder)
     }
+}
+
+// create Meetings
+for (let i = 0; i < numMeetings; i++) {
+    const id = uuidv4()
+    const PK = `Meeting-${id}`
+    const SK = `Meeting-${id}`
+
+    const meeting = {
+        PK, SK
+    }
+
+    vData.push(meeting)
+    meetings.push(meeting)
+
+    addTo('company', meeting)
+}
+
+
+// // create/attach Agreements
+for (let i = 0; i < numAgreements; i++) {
+    const id = uuidv4()
+    const PK = `Agreement-${id}`
+    const SK = `Agreement-${id}`
+    const agreementTypeNum = Math.round(Math.random()*(agreementTypes.length-1));
+    const name = agreementTypes[agreementTypeNum]
+
+    const agreement = {
+        PK, SK, name
+    }
+    agreements.push(agreement)
+    addTo('meeting', agreement)
+}
+
+// create/attach Votes
+for (let i = 0; i < numVotes; i++) {
+    const id = uuidv4()
+    const PK = `Vote-${id}`
+    const SK = `Vote-${id}`
+
+    const vote = {
+        PK, SK
+    }
+    votes.push(vote)
+    addTo('agreement', vote)
 }
 
 const vFile = 'vysae.json';
