0b24eb9f0a509ff724f50fd837470bc60006ca30
knowledge91 <dirkhornung91@gmail.com>
2018-08-31 18:13:25 +0200

    refactored create user

--- backend/handler.js
+++ backend/handler.js
@@ -1,25 +1,6 @@
 import docx from 'docxpresso'
-import Amplify, { Auth } from 'aws-amplify'
-import generator from 'generate-password'
-// node 6... has no fetch
-import fetch from 'node-fetch'
-global.fetch = require('node-fetch')
-import AWS from 'aws-sdk';
 import sendMail from './services/sendMail'
-AWS.config.update({ region: 'eu-west-1' })
-AWS.config.setPromisesDependency(require('bluebird'));
-
-const docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
-
-
-Amplify.configure({
-  Auth: {
-    identityPoolId: 'eu-west-1:5e331c35-306a-4885-9044-383af2ad1067',
-    region: 'eu-west-1',
-    userPoolId: 'eu-west-1_MUEHMQ2R2',
-    userPoolWebClientId: '675m7ktuif5or2ci1nq9tg1nu9'
-  }
-})
+import createUser from './services/createUser'
 
 module.exports.meetingDocx = (event, context, callback) => {
   const url = docx.previewDocument(
@@ -45,46 +26,18 @@ module.exports.conveneMeeting = (event, context, callback) => {
 };
 
 // create user
-module.exports.createuser = async (event, context, callback) => {
+module.exports.createUser = async (event, context, callback) => {
   context.callbackwaitsforemptyeventloop = false;
 
   const { username, email, phone_number } = event.arguments
 
-  // create user in userpool
+  const userInput = { username, email, phone_number }
   try {
-    const authresult = await auth.signup({
-      username: username,
-      password: generator.generate({ length: 8, numbers: true, symbols: true, strict: true }),
-      attributes: {
-        email: email,
-        phone_number: phone_number,
-      }
-    })
-    const { usersub } = authresult
-
-    // create user in dynamodb
-    await docclient.put({
-      tablename: 'vysaeuser',
-      item: {
-        'userid': usersub
-      }
-    }).promise()
-
-    callback(null, {
-      userid: usersub,
-      username,
-      email,
-      phone_number
-    })
+    const user = await createUser(userInput)
+    console.log(user)
   }
-  catch (err) {
-    console.error('error \n', err, '\n')
-    callback(null, {
-      userid: null,
-      username: null,
-      email: null,
-      phone_number: null
-    })
+  catch(err) {
+    console.error(err)
   }
 }
 
---
+++ backend/services/createUser.js
@@ -0,0 +1,59 @@
+import Amplify, { Auth } from 'aws-amplify'
+import generator from 'generate-password'
+// node 6... has no fetch
+import fetch from 'node-fetch'
+global.fetch = fetch
+import AWS from 'aws-sdk';
+
+AWS.config.update({ region: 'eu-west-1' })
+AWS.config.setPromisesDependency(require('bluebird'));
+const docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'eu-west-1:5e331c35-306a-4885-9044-383af2ad1067',
+    region: 'eu-west-1',
+    userPoolId: 'eu-west-1_MUEHMQ2R2',
+    userPoolWebClientId: '675m7ktuif5or2ci1nq9tg1nu9'
+  }
+})
+
+export default (userInput) => {
+  const { username, email, phone_number } = userInput
+  const password = generator.generate({ length: 8, numbers: true, symbols: true, strict: true })
+
+  return new Promise( async (resolve, reject) => {
+
+    // create user in userpool
+    try {
+      const authResult = await Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email,
+          phone_number,
+        }
+      })
+      const { userSub } = authResult
+
+      // create user in dynamodb
+      await docClient.put({
+        TableName: 'VysaeUser',
+        Item: {
+          'userId': userSub
+        }
+      }).promise()
+
+      resolve({
+        userId: userSub,
+        username,
+        password,
+        email,
+        phone_number
+      })
+    }
+    catch (err) {
+      reject(err)
+    }
+  })
+}
