92b354dc9a460c17f549ebe33ef5b3296c56b71a
knowledge91 <dirkhornung91@gmail.com>
2018-09-10 11:13:53 +0200

    update company basics mutation

--- frontend/src/queries/MutationUpdateCompany.js
+++ frontend/src/queries/MutationUpdateCompany.js
@@ -1,12 +1,17 @@
 import gql from 'graphql-tag'
 
 export default gql(`
-mutation updateCompany {
+mutation updateCompany(
+  $companyId: ID!
+  $name: String!
+  $placeOfBusiness: String!
+  $nif: String!
+) {
   updateCompany(
-    companyId:  "89c01be1-3f6c-4170-b95b-c6686c9f9c4f"
-    name: "oook"
-    placeOfBusiness: "fulda"
-    nif: "sdf"
+    companyId: $companyId
+    name: $name
+    placeOfBusiness: $placeOfBusiness
+    nif: $nif
   ) {
     companyId
     name
