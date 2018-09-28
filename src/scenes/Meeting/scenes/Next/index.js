b230289e78b91b13c05f98c30faa14567b33bd2f
knowledge91 <dirkhornung91@gmail.com>
2018-09-28 11:26:39 +0200

    removed vote result

--- src/scenes/Meeting/scenes/Next/index.js
+++ src/scenes/Meeting/scenes/Next/index.js
@@ -46,8 +46,8 @@ function Next(props) {
       <Fragment>
         {/* <Link to="/meetings/pdf" onClick={() => selectMeeting(record)}>Leer orden del día</Link> */}
         <Link to={`/${companyId}/meeting/${record.id}/vote`}>Votar</Link>
-        <Divider type="vertical"/>
-        <Link to={`/${companyId}/meeting/${record.meetingId}/result`}>Resultado</Link>
+        {/* <Divider type="vertical"/>
+            <Link to={`/${companyId}/meeting/${record.meetingId}/result`}>Resultado</Link> */}
       </Fragment>
     )
   }]
