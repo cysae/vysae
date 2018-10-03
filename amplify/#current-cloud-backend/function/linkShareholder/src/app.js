6708a4c3d671ec577a616962870febfeeb5b8e00
Dirk Hornung <dirkhornung91@gmail.com>
2018-10-03 16:41:12 +0200

    meetingAgreements with majority info

--- amplify/#current-cloud-backend/function/linkShareholder/src/app.js
+++ amplify/#current-cloud-backend/function/linkShareholder/src/app.js
@@ -10,7 +10,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const sendMail = require('./sendMail.js');
-const htmlToPdf = requier('./htmlToPdf.js')
+const htmlToPdf = require('./htmlToPdf.js')
 
 // declare a new express app
 var app = express()
@@ -35,11 +35,6 @@ app.get('/linkShareholder', function(req, res) {
   res.json({success: 'get call succeed!', url: req.url});
 });
 
-app.get('/items/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
-
 /****************************
 * Example post method *
 ****************************/
@@ -53,8 +48,13 @@ app.post('/linkShareholder', async function(req, res) {
 });
 
 app.post('/acta', function(req, res) {
-
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  htmlToPdf('<body>Hello World</body>', (pdf) => {
+    const filename = 'acta.pdf'
+    res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+    res.setHeader('Content-type', 'application/pdf');
+    res.setHeader('isBase64Encoded', true);//isBase64Encoded: true
+    res.status(200).send(pdf)
+  })
 });
 
 /****************************
--- amplify/#current-cloud-backend/function/linkShareholder/src/htmlToPdf.js
+++ amplify/#current-cloud-backend/function/linkShareholder/src/htmlToPdf.js
@@ -1,9 +1,14 @@
-var wkhtmltpdf = require('wkhtmltopdf');
+var wkhtmltopdf = require('wkhtmltopdf');
 var MemoryStream = require('memorystream');
 
-function htmlToPdf(html) {
-  wkhtmltopdf(html_utf8, event.options, function(code, signal) {
+const memStream = new MemoryStream();
+
+function htmlToPdf(html, callback) {
+  wkhtmltopdf(html, {}, function(code, signal) {
     var pdf = memStream.read();
-    return pdf
+
+    callback(pdf)
   }).pipe(memStream);
 }
+
+module.exports = htmlToPdf
